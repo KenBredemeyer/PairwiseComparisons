0d2f4244a6283007bf38b228fee910e89b0462e6
Ken Bredemeyer <ken.bredemeyer@uwa.edu.au>
2018-06-26 11:02:24 +0800

    add complete_comparisons2 to recognise judges and return the full data.frame when adding zero comparisons.

---
+++ R/data_array.R
@@ -0,0 +1,23 @@
+complete_comparisons2 <- function(x) {
+	zero_win_rows <- vector()
+	j <- 1
+	Judges <- unique(test_tile_tower$Judge)
+	for (i in 1:nrow(x)) {
+		judge <- x[i, "Judge"]
+		if(!any(x[x$Judge == judge , "Item.1"] == x[i, "Item"] & x[x$Judge == judge , "Item"] == x[i, "Item.1"])) {
+	    zero_win_rows[j] <- i
+	    j <- j+1
+		}
+	}
+	zero_wins <- data.frame(matrix(ncol = ncol(x)))
+	colnames(zero_wins) <- colnames(x)
+	zero_wins[(1:j-1), c("Item", "Item.1")] <- x[zero_win_rows, c("Item.1", "Item")]
+	zero_wins[ , "Selected"] <- 0
+	zero_wins[ , -which(names(zero_wins) %in% c("Item", "Item.1", "Selected"))] <- x[ , -which(names(x) %in% c("Item", "Item.1", "Selected"))]
+	if (j > 1) {
+		x <- rbind(x, zero_wins)
+	}
+  x
+}
+
+## build in criteria ! ##
