80ddcc9d63bb18ddeb3f6975615b63351ebd069a
Ken Bredemeyer <ken.bredemeyer@uwa.edu.au>
2018-06-26 15:05:57 +0800

    enable pairs_format2() to return a 3D array by summing across judges or criteria

--- R/data_array.R
+++ R/data_array.R
@@ -1,3 +1,56 @@
+#' Create data matrix for estimation
+#'
+#' Sum across criteria to estimate judge discrimination, and sum across judges
+#' to estimate criteria discrimination.
+#'
+#' @param x data.frame containing the variables \code{Judge, Criteria, Item,
+#'   Item.1, Selected}.
+#' @param judges Numeric or character vector specifying judges to include.
+#' @param criteria Numeric or character vector specifying criteria to include.
+#' @param sum_across Character vector. \code{"judges"}, \code{"criteria"} or
+#'   \code{c("judges", "criteria")}
+#'
+#' @return A data matrix (2D) if \code{sum_across = "judges"} or
+#'   \code{sum_across = "criteria"}, or if \code{sum_across = c("judges",
+#'   "criteria")} then a 3D array is returned.
+#' @export
+pairs_format2 <- function(x, judges = "all", criteria = "all", sum_across = c("judges", "criteria")) {
+	options(stringsAsFactors = FALSE)
+	# allow a 2D or 3D array to be returned.  Must sum across judges, or criteria, or both
+	stopifnot(any(sum_across == "judges") | any(sum_across == "criteria"))
+	if (length(judges) == 1 && judges == "all") judges <- judges(x)
+	if (length(criteria) == 1 && criteria == "all") criteria <- criteria(x)
+	x <- pref_codes(x)
+	x <- left_preferred(x)
+  x <- complete_comparisons2(x)
+  x <- reshape2::acast(x,
+	                Item ~ Item.1 ~ Judge ~ Criteria,
+	                drop = FALSE,
+	                value.var = "Selected")
+  x <- x[ , , judges, criteria]
+  if (any(sum_across == "judges") && any(sum_across == "criteria")) {
+	  # flatten to data matrix
+	  x <- apply(x, 1:2, sum2)
+  } else if (all(sum_across == "judges")) {
+  	x <- apply(x, c(1,2,4), sum2)
+  } else if (all(sum_across == "criteria")) {
+  	x <- apply(x, 1:3, sum2)
+  }
+  x
+}
+
+
+#' List all judges
+#' @param x data.frame containing the variable \code{Judge}
+#' @export
+judges <- function(x) unique(x$Judge)
+
+#' List all criteria
+#' @param x data.frame containing the variable \code{Criteria}
+#' @export
+criteria <- function(x) unique(x$Criteria)
+
+
 complete_comparisons2 <- function(x) {
 	zero_win_rows <- vector()
 	j <- 1
@@ -21,39 +74,4 @@ complete_comparisons2 <- function(x) {
 	}
   x
 }
-
-#' Create data matrix for estimation
-#'
-#' @param x data.frame containing the variables \code{Judge, Criteria, Item,
-#'   Item.1, Selected}.
-#' @param judges Numeric or character vector specifying judges to include.
-#' @param criteria Numeric or character vector specifying criteria to include.
-#' @export
-pairs_format2 <- function(x, judges = "all", criteria = "all") {
-	options(stringsAsFactors = FALSE)
-	if (length(judges) == 1 && judges == "all") judges <- judges(x)
-	if (length(criteria) == 1 && criteria == "all") criteria <- criteria(x)
-	x <- pref_codes(x)
-	x <- left_preferred(x)
-  x <- complete_comparisons2(x)
-  x <- reshape2::acast(x,
-	                Item ~ Item.1 ~ Judge ~ Criteria,
-	                drop = FALSE,
-	                value.var = "Selected")
-  x <- x[ , , judges, criteria]
-  # flatten to data matrix
-  x <- apply(x, 1:2, sum2)
-  x
-}
-
-#' List all judges
-#' @param x data.frame containing the variable \code{Judge}
-#' @export
-judges <- function(x) unique(x$Judge)
-
-#' List all criteria
-#' @param x data.frame containing the variable \code{Criteria}
-#' @export
-criteria <- function(x) unique(x$Criteria)
-
 
