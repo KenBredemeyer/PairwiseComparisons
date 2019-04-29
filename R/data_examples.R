bfb7082e5b557f315ad8031ccdedab184ae6fef4
Ken Bredemeyer <ken.bredemeyer@uwa.edu.au>
2019-04-29 11:30:05 +0800

    add GTPA data example.

---
+++ R/data_examples.R
@@ -0,0 +1,8 @@
+#' Graduate Teacher Performance Assessment 2018 Pairwise Comparison Project
+#'
+#' @docType data
+#'
+#' @usage data(gtpa18)
+#'
+#' @format A data frame of pairwise comparisons
+"gtpa18"
