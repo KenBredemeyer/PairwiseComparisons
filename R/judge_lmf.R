b49686f442cdcc36a88e37d8e248d191e009a0a8
Ken Bredemeyer <ken.bredemeyer@uwa.edu.au>
2019-08-14 16:21:11 +0800

    Complete difficulty section of judge_lmf.

--- R/judge_lmf.R
+++ R/judge_lmf.R
@@ -1,3 +1,9 @@
+## judge_lmf()
+# args
+x
+convergence_criteria = c(0.01, 0.01, 0.01, 0.01, 0.001)
+max_iterations = c(outer_loop = 20, beta_loop = 20, alpha_loop = 20, inner_loop = 20)
+
 # @param x 3D array: 3rd D judges
 npersons <- dim(x)[1]
 njudges <- dim(x)[3]
@@ -15,26 +21,36 @@ se_beta <- rep(0, npersons)
 se_alpha <- rep(1, njudges)
 
 # initialise convergence matrices
+convergence_beta_inner <- matrix(NA, nrow = max_iterations["inner_loop"], ncol = dim(x)[2])
+convergence_alpha_inner <- matrix(NA, nrow = max_iterations["inner_loop"], ncol = dim(x)[3])
+convergence_beta <- matrix(NA, nrow = max_iterations["beta_loop"], ncol = dim(x)[2]) # betas from the outer loop
+convergence_alpha <- matrix(NA, nrow = max_iterations["alpha_loop"], ncol = dim(x)[3]) # alphas from the outer loop
+convergence_beta[1, ] <- 0
+convergence_alpha[1, ] <- 1
 
 # initialise standard error objects
 se_beta <- matrix(NA, nrow = npersons, ncol = 1)
 se_alpha <- matrix(NA, nrow = njudges, ncol = 1)
 
 
-# difficulty
- for (n in seq_len(npersons)) {    # person loop
-  inner loop {
-    for (alpha_i in 1:njudges) {   # gather information across "virtual items"
-    comparisons_i_judge[[alpha_i]] <- which(!is.na(x[n, , alpha_i]))
-    probs_judge[[alpha_i]] <- exp(alpha[alpha_i] * (beta[n] - beta_m)) /
-    	                   (1 + exp(alpha[alpha_i] * (beta[n] - beta_m)))
-    dm_counts_judge[[alpha_i]] <- x[item_i , , alpha_i]
-    involved_judge[[alpha_i]] <- na.omit(x[item_i , , alpha_i]) + na.omit(x[ , item_i, alpha_i])
+# outer loop
 
-    fp_beta_judge[[alpha_i]] <- alpha[[alpha_i]] * sum(involved_judge[[alpha_i]] * probs_judge[[alpha_i]])
-                          - alpha[[alpha_i]] * sum(dm_counts_judge[[alpha_i]], na.rm = TRUE)
-    fpp_beta_judge[[alpha_i]] <- alpha[[alpha_i]]^2 * sum(involved_judge[[alpha_i]] * probs_judge[[alpha_i]] * (1 - probs_judge[[alpha_i]])
-    }
+# difficulty ------------------------------------------------------------------------------
+for (beta_loop_i in 2:max_iterations["beta_loop"]) {
+  for (n in seq_len(npersons)) {    # person loop
+    for (i in 2:max_iterations["inner_loop"]) {   # inner loop
+      for (alpha_i in 1:njudges) {   # gather information across "virtual items"
+        comparisons_i_judge[[alpha_i]] <- which(!is.na(x[n, , alpha_i]))
+        probs_judge[[alpha_i]] <- exp(alpha[alpha_i] * (beta[n] - beta_m)) /
+            	                   (1 + exp(alpha[alpha_i] * (beta[n] - beta_m)))
+        dm_counts_judge[[alpha_i]] <- x[item_i , , alpha_i]
+        involved_judge[[alpha_i]] <- na.omit(x[item_i , , alpha_i]) + na.omit(x[ , item_i, alpha_i])
+
+        fp_beta_judge[[alpha_i]] <- alpha[[alpha_i]] * sum(involved_judge[[alpha_i]] * probs_judge[[alpha_i]])
+                                  - alpha[[alpha_i]] * sum(dm_counts_judge[[alpha_i]], na.rm = TRUE)
+        fpp_beta_judge[[alpha_i]] <- alpha[[alpha_i]]^2 * sum(involved_judge[[alpha_i]] *
+                                             probs_judge[[alpha_i]] * (1 - probs_judge[[alpha_i]]))
+      }
     # combine data across judges
     comparisons_i <- do.call(c, comparisons_i_judge)
     comparisons_count <- lapply(comparisons_i_judge, length)
@@ -49,15 +65,22 @@ se_alpha <- matrix(NA, nrow = njudges, ncol = 1)
     beta[n] <- beta[n] - fp_beta / fpp_beta
 
     # inner loop convergence
-
+    convergence_beta_inner[i, n] <- beta[n]
+    # stopping criterion
+    if (!is.na(convergence_beta_inner[i, n]) && !is.na(convergence_beta_inner[i-1, n]) &&
+        abs(convergence_beta_inner[i, n] - convergence_beta_inner[i-1, n]) <= convergence_criteria[1]) {
+      break
+    }
   }
-  se_beta[n, ] <- 1 / sqrt(fpp_beta)
-
+    se_beta[n, ] <- 1 / sqrt(fpp_beta)
+  }
   beta <- beta - mean(beta)
-
-  # beta convergence
-
+  # beta convergence and stopping criterion
+  convergence_beta[beta_loop_i-1, ] <- beta
+  if (!any(is.na(convergence_beat[beta_loop_i, ])) && !any(is.na(convergence_beta[beta_loop_i -1, ])) &&
+    max(abs(convergence_beta[beta_loop_i, ] - convergence_beta[beta_loop_i -1, ])) < convergence_criteria[2]) {
+    break
+  }
 }
 
-
-# discrimination
+# discrimination -------------------------------------------------------------------------------------
